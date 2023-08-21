e99240ca34b40f5fccfd8f32f3cede5243f34d46
janganboy <sipadil28@hotmail.com>
2023-08-21 19:46:40 +0700

    Modified: Feed and PromptCard content | Added: Feed route to show PromptCard content

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -14,7 +14,6 @@ const nav = () => {
   useEffect(() => {
     const setUpProviders = async () => {
       const response = await getProviders();
-      
       setProviders(response);
     }
     setUpProviders();
